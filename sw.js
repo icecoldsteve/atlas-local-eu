e58ce9ba42b4d5422417c2232d2ffc861bb4faa5
SMSAK <39767928+icecoldsteve@users.noreply.github.com>
2026-09-06 07:30:21 +0200

    Cache data.js in the service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "atlas-local-eu-v2";
-const ASSETS = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "atlas-local-eu-v3";
+const ASSETS = ["/", "/index.html", "/styles.css", "/data.js", "/app.js", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
